2e67cc87d450e13ab8561894198e6f039bc9cbc1
Micha Mailänder <micha@mailaender.me>
2023-03-17 14:27:13 +0000

    Fixed Tailwind

--- components/testComponent/index.tsx
+++ components/testComponent/index.tsx
@@ -1,12 +1,15 @@
 export default function TestComponent() {
   return (
     <>
-      <button
-        type="button"
-        className="rounded-md bg-indigo-600 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      <div className="text-3xl text-black dark:text-white">
+        The first No-GraphQL GraphQL client
+      </div>
+      <a
+        href="/#playground"
+        className="rounded-full bg-indigo-600 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
-        Button text
-      </button>
+        Playground
+      </a>
     </>
   );
 }
