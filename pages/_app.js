675e09b7cfca0ae0b6ec43ea98ac81557063b36a
Micha Mailänder <micha@mailaender.me>
2023-03-17 19:16:59 +0000

    Installed Reshaped

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import "/styles.css";
+import { Reshaped } from "reshaped/bundle";
+import "reshaped/themes/reshaped/theme.css";
+import "reshaped/bundle.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Reshaped theme="reshaped">
+      <Component {...pageProps} />
+    </Reshaped>
+  );
 }
