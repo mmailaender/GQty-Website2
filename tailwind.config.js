d87d353103bf5e6c33550b4a3b2d96ed769110f9
bohdank1995 <bohdan.karpliak@dechea.com>
2023-03-17 13:49:03 +0000

    GQty colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,8 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          light: "",
-          dark: "",
+          light: "#EF46BC",
+          dark: "#E767C0",
         },
         secondary: {
           light: "",
@@ -24,20 +24,43 @@ module.exports = {
           dark: "",
         },
         success: {
-          light: "",
-          dark: "",
+          light: "#23A30F",
+          dark: "#85F273",
         },
         warning: {
-          light: "",
-          dark: "",
+          light: "#F5D422",
+          dark: "#FFEB7C",
         },
         error: {
-          light: "",
-          dark: "",
+          light: "#EF3726",
+          dark: "#FF8579",
         },
         surface: {
-          light: "",
-          dark: "",
+          light: {
+            50: "#FBF9FA",
+            100: "#E2E0E2",
+            200: "#CAC8C9",
+            300: "#B2AFB1",
+            400: "#999798",
+            500: "#807E80",
+            600: "#686667",
+            700: "#504D4F",
+            800: "#373536",
+            900: "#201C1E",
+          },
+
+          dark: {
+            50: "#151215",
+            100: "#2E2B2E",
+            200: "#474447",
+            300: "#605E60",
+            400: "#797779",
+            500: "#929091",
+            600: "#ABA9AA",
+            700: "#C3C3C3",
+            800: "#DCDCDC",
+            900: "#F5F5F5",
+          },
         },
       },
     },
