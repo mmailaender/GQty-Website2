9494cd7ba152afc9d7291615798e781dad02fca1
Micha Mailänder <micha@mailaender.me>
2023-03-17 17:32:49 +0000

    Increased Gap between USPs

--- components/USP/index.tsx
+++ components/USP/index.tsx
@@ -4,7 +4,7 @@ import Write from "./write";
 export default function USP() {
   return (
     <>
-      <div className="flex flex-col gap-2 pt-6">
+      <div className="flex flex-col gap-52 pt-6">
         <Read />
         <Write />
       </div>
