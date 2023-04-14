0f361da7cf6ba76e83a4fc478f98ae7ac7c8a3ac
bohdank1995 <bohdan.karpliak@dechea.com>
2023-04-14 08:53:35 +0000

    Hero section updates

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -1,6 +1,27 @@
-import { Image, Text, View } from "reshaped/bundle";
+import {
+  Image,
+  Text,
+  View,
+  Button,
+  ButtonProps,
+  Link,
+  LinkProps,
+} from "reshaped/bundle";
+import Copy from "../Icons/Play/Copy";
+
+function copyToClipboard(textToCopy: string): void {
+  navigator.clipboard
+    .writeText(textToCopy)
+    .then(() => {
+      alert("Text copied to clipboard!");
+    })
+    .catch((error) => {
+      console.error("Failed to copy text: ", error);
+    });
+}
 
 export default function HeroSection() {
+  const npmCommand = "npx @gqty/cli";
   return (
     <>
       <View
@@ -8,15 +29,38 @@ export default function HeroSection() {
         align="center"
         justify="start"
         gap={26}
-        paddingTop={12}
+        paddingTop={20}
       >
-        <View direction="column" align="center" gap={5}>
-          <Text variant="display-2" align="center">
-            The No-GraphQL client
-          </Text>
-          <Text variant="featured-1" align="center">
-            for TypeScript
-          </Text>
+        <View direction="column" align="center" gap={8}>
+          <View direction="column" align="center" gap={4}>
+            <Text variant="display-2" align="center">
+              The No-GraphQL client
+            </Text>
+            <Text variant="featured-2" align="center">
+              for TypeScript
+            </Text>
+          </View>
+          <View
+            paddingTop={4}
+            paddingBottom={12}
+            width="100%"
+            maxWidth="160px"
+            direction="column"
+            align="center"
+            gap={3}
+          >
+            <Button color="white" rounded size="large" fullWidth>
+              Get Started
+            </Button>
+            <Link
+              onClick={() => copyToClipboard(npmCommand)}
+              color="inherit"
+              variant="plain"
+              icon={<Copy />}
+            >
+              npx @gqty/cli
+            </Link>
+          </View>
         </View>
         <div className="relative">
           <View direction="row" gap={5} width="100%">
@@ -35,7 +79,7 @@ export default function HeroSection() {
                 <Image src="/before.png" width="484px" height="100%"></Image>
               </View>
             </View.Item>
-            <div className="absolute bottom-0 left-0 h-40 w-full bg-gradient-to-t from-[#0D1117] via-[#0D1117]"></div>
+            <div className="absolute bottom-0 left-0 h-40 w-full bg-gradient-to-t from-[#111111] via-[#111111]"></div>
           </View>
         </div>
       </View>
