434fd1bd456e0075e48817f96a5e4fb98c7f571f
Micha Mailänder <micha@mailaender.me>
2023-04-24 15:37:18 +0000

    Added position: sticky (Waiting for Nextra fix)

--- reshaped.config.js
+++ reshaped.config.js
@@ -3,6 +3,7 @@ const config = {
     gqty: {
       color: {
         backgroundPrimary: { hex: "#E767C0", hexDark: "#E767C0" },
+        backgroundBase: { hex: "#151215", hexDark: "#151215"}
       },
     },
   },
