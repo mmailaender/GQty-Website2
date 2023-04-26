1d76d11415894f0efd742095ae8348830e0eac84
bohdank1995 <bohdan.karpliak@dechea.com>
2023-04-26 12:05:13 +0000

    Hero Section

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -41,61 +41,102 @@ export default function HeroSection() {
   return (
     <>
       <div className="flex flex-col bg-surface-900 w-full h-screen justify-between content-center">
-        <View align="center" paddingTop={{ xl: 20, l: 20, m: 12, s: 8 }}>
+        <View
+          align="center"
+          gap={4}
+          paddingTop={{ xl: 20, l: 20, m: 12, s: 8 }}
+        >
           <Text variant="display-2" align="center">
             The No-GraphQL client
           </Text>
-          <Text align="center">for TypeScript</Text>
-        </View>
-
-        {/* Mobile image */}
-        <View
-          direction="column"
-          position="relative"
-          align="center"
-          paddingTop={28}
-          gap={4}
-        >
-          {/* Image */}
-          <View position="relative">
-            <View paddingBottom={16}>
-              <Image
-                src="/before-after.png"
-                width="700px"
-                className="scale-150"
-              />
-            </View>
-            <div className="absolute bottom-0 left-0 h-40 w-full bg-gradient-to-t from-surface-900 via-surface-900"></div>
-          </View>
-        </View>
-        {/* CTA */}
-        <View
-          direction="row"
-          justify="center"
-          align="center"
-          gap={8}
-          paddingBottom={24}
-        >
-          <Link
-            onClick={handleCopyClick}
-            color="inherit"
-            variant="plain"
-            icon={<Copy />}
-          >
-            npx @gqty/cli
-          </Link>
-          <View>
-            <Button
-              color="white"
-              rounded
-              size="xlarge"
-              fullWidth
-              href="/getting-started"
+          <Text variant="featured-2" align="center">
+            for TypeScript
+          </Text>
+          <View paddingTop={4} align="center" gap={3}>
+            <Hidden hide={{ xl: false, l: false, m: true, s: true }}>
+              <View width="200px" maxWidth="240px" paddingTop={4}>
+                <Button
+                  color="white"
+                  rounded
+                  size="xlarge"
+                  fullWidth={true}
+                  href="/getting-started"
+                >
+                  Get Started
+                </Button>
+              </View>
+            </Hidden>
+            <Link
+              onClick={handleCopyClick}
+              color="inherit"
+              variant="plain"
+              icon={<Copy />}
             >
-              Get Started
-            </Button>
+              npx @gqty/cli
+            </Link>
           </View>
         </View>
+        {/* Desktop image */}
+        <Hidden hide={{ xl: false, l: false, m: true, s: true }}>
+          <View direction="row" gap={8} paddingBottom={24} position="relative">
+            <View.Item columns={5}>
+              <View>
+                <Image src="/before.png" />
+              </View>
+            </View.Item>
+            <View.Item columns={2}>
+              <View>
+                <Image src="/arrow.svg" />
+              </View>
+            </View.Item>
+            <View.Item columns={5}>
+              <View>
+                <Image src="/after.png" />
+              </View>
+            </View.Item>
+            <div className="absolute bottom-0 left-0 h-60 w-full bg-gradient-to-t from-surface-900 via-surface-900"></div>
+          </View>
+        </Hidden>
+        {/* Mobile image */}
+        <Hidden hide={{ xl: true, l: true, m: false, s: false }}>
+          <View
+            direction="column"
+            position="relative"
+            align="center"
+            paddingTop={28}
+            gap={4}
+          >
+            {/* Image */}
+            <View position="relative" className="scale-150">
+              <View paddingBottom={16} maxWidth="400px">
+                <Image src="/before-after.png" width="588px" />
+              </View>
+              <div className="absolute bottom-0 left-0 h-40 w-full bg-gradient-to-t from-surface-900 via-surface-900"></div>
+            </View>
+          </View>
+        </Hidden>
+        {/* Mobile CTA */}
+        <Hidden hide={{ xl: true, l: true, m: false, s: false }}>
+          <View
+            direction="row"
+            justify="center"
+            align="center"
+            gap={8}
+            paddingBottom={28}
+          >
+            <View>
+              <Button
+                color="white"
+                rounded
+                size="xlarge"
+                fullWidth
+                href="/getting-started"
+              >
+                Get Started
+              </Button>
+            </View>
+          </View>
+        </Hidden>
       </div>
     </>
   );
