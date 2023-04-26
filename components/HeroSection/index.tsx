2b4fecd097a98125ce8d7c3abfec6f89f258175e
bohdank1995 <bohdan.karpliak@dechea.com>
2023-04-26 10:56:24 +0000

    Hero changes (WIP)

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -40,142 +40,67 @@ export default function HeroSection() {
 
   return (
     <>
-      <View
-        position="relative"
-        direction="column"
-        align="center"
-        justify="start"
-        paddingTop={20}
-        className="h-screen"
-      >
-        <div className="absolute rounded-full aspect-square top-[-100px] filter blur-[200px] overflow-hidden z-0">
-          <View width={90} height={40} className="bg-[#DA58B3] "></View>
-        </div>
+      <div className="flex flex-col bg-surface-900 w-full h-screen justify-between content-center">
+        <View align="center" paddingTop={{ xl: 20, l: 20, m: 12, s: 8 }}>
+          <Text variant="display-2" align="center">
+            The No-GraphQL client
+          </Text>
+          <Text align="center">for TypeScript</Text>
+        </View>
+
+        {/* Mobile image */}
         <View
           direction="column"
+          position="relative"
           align="center"
-          gap={{ l: 8, s: 4 }}
-          width="100%"
-          className="h-screen 2xl:h-16"
+          paddingTop={28}
+          gap={4}
         >
-          <View direction="column" align="center" gap={4} width="100%">
-            <Text variant="display-2" align="center">
-              The No-GraphQL client
-            </Text>
-            <Text variant="featured-2" align="center">
-              for TypeScript
-            </Text>
-          </View>
-
-          {/* Desktop CTA */}
-          <Hidden hide={{ s: true, m: true, l: false, xl: false }}>
-            <View
-              paddingTop={4}
-              paddingBottom={12}
-              width="100%"
-              maxWidth="240px"
-              direction="column"
-              align="center"
-              gap={3}
-            >
-              <Button
-                color="white"
-                rounded
-                size="large"
-                fullWidth
-                href="/getting-started"
-              >
-                Get Started
-              </Button>
-              <Link
-                onClick={handleCopyClick}
-                color="inherit"
-                variant="plain"
-                icon={<Copy />}
-              >
-                npx @gqty/cli
-              </Link>
+          {/* Image */}
+          <View position="relative">
+            <View paddingBottom={16}>
+              <Image
+                src="/before-after.png"
+                width="700px"
+                className="scale-150"
+              />
             </View>
-          </Hidden>
+            <div className="absolute bottom-0 left-0 h-40 w-full bg-gradient-to-t from-surface-900 via-surface-900"></div>
+          </View>
         </View>
-        {/* Images */}
-        <div className="relative">
-          {/* Desktop */}
-          <Hidden hide={{ s: true, m: true, l: false }}>
-            <View direction="row" gap={5} width="100%">
-              <View.Item columns={5}>
-                <View>
-                  <Image src="/before.png" width="484px" height="100%"></Image>
-                </View>
-              </View.Item>
-              <View.Item columns={2}>
-                <View align="center">
-                  <Image src="/arrow.svg"></Image>
-                </View>
-              </View.Item>
-              <View.Item columns={5}>
-                <View>
-                  <Image src="/after.png" width="484px" height="100%"></Image>
-                </View>
-              </View.Item>
-              <div className="absolute bottom-0 left-0 h-40 w-full bg-gradient-to-t from-surface-900 via-surface-900"></div>
-            </View>
-          </Hidden>
-
-          {/* Mobile */}
-          <Hidden hide={{ s: false, m: false, l: true, xl: true }}>
-            <View direction="row" gap={5} width="100%">
-              <View paddingBottom={{ s: 24, m: 24, l: 8, xl: 8 }}>
-                <Image
-                  src="/before-after.png"
-                  width="588px"
-                  height="100%"
-                ></Image>
-              </View>
-
-              <div className="absolute bottom-20 left-0 h-40 w-full bg-gradient-to-t from-surface-900 via-surface-900"></div>
-            </View>
-          </Hidden>
-        </div>
-        {/* Mob CTA */}
-        <Hidden hide={{ s: false, m: false, l: true, xl: true }}>
-          <View
-            padding={[4, 8]}
-            direction="row"
-            align="center"
-            justify="center"
-            gap={6}
-            width="100%"
-            position="fixed"
-            insetBottom={0}
-            insetStart={0}
-            zIndex={2}
-            className="bg-surface-900 opacity-95 filter backdrop-blur-sm"
+        {/* CTA */}
+        <View
+          direction="row"
+          justify="center"
+          align="center"
+          gap={8}
+          paddingBottom={24}
+        >
+          <Link
+            onClick={handleCopyClick}
+            color="inherit"
+            variant="plain"
+            icon={<Copy />}
           >
-            <View.Item grow>
-              <Button
-                color="white"
-                rounded
-                size="large"
-                fullWidth
-                href="/getting-started"
-              >
-                Get Started
-              </Button>
-            </View.Item>
-            <View align="center">
-              <Link
-                onClick={handleCopyClick}
-                color="inherit"
-                variant="plain"
-                icon={<Copy />}
-              >
-                npx @gqty/cli
-              </Link>
-            </View>
+            npx @gqty/cli
+          </Link>
+          <View>
+            <Button
+              color="white"
+              rounded
+              size="xlarge"
+              fullWidth
+              href="/getting-started"
+            >
+              Get Started
+            </Button>
           </View>
-        </Hidden>
-      </View>
+        </View>
+      </div>
     </>
   );
+}
+
+{
+  /* <div className="absolute bottom-0 left-0 h-40 w-full bg-gradient-to-t from-surface-900 via-surface-900"></div> */
 }
