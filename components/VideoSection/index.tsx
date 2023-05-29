b22fada34bc360fccdf42af6b8227a3bef560651
bohdank1995 <bohdan.karpliak@dechea.com>
2023-05-29 08:20:43 +0000

    Updated Dependencies

--- components/VideoSection/index.tsx
+++ components/VideoSection/index.tsx
@@ -12,19 +12,20 @@ const VideoSection = () => {
   return (
     <>    
     <div className=" flex flex-col w-full bg-surface-800 items-center justify-center pt-20">
-      <div className=" max-w-[1270px]  grid grid-cols-12 gap-6">
+      <div className=" max-w-[1108px]  grid grid-cols-12 gap-6">
         {/* title */}
-        <div className="col-start-4 col-span-6">
+        <div className="col-start-4 col-span-6 w-full">
           <View gap={6}>
 <Text variant="display-3" align="center">Simplify Your Life</Text>
 <Text variant="body-1"  align="center" className="text-surface-200">Streamline your coding process, save time, and implement our innovative solution to say goodbye to tedious coding once and for all!</Text>
 </View>
 </div>
-<div className="col-start-1 col-span-3"><Text variant="featured-2">GQty Basics</Text></div>
+<div className="col-start-1 col-span-3">
+  <View gap={2}><Text variant="featured-2">GQty Basics</Text><Text variant="body-2" color="neutral-faded" weight="medium">9 Tutorials</Text></View></div>
 </div>
 
     </div>
-    <div></div>
+    
     </>
 
   );
